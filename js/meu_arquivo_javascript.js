0dee03b2a656d7260954530f62265094e9e14923
PEDRO HENRIQUE AMANCIO DOS SANTOS ROSA <pedro.h.rosa10@aluno.senai.br>
2025-02-19 20:11:16 -0300

    DOM

--- js/meu_arquivo_javascript.js
+++ js/meu_arquivo_javascript.js
@@ -10,4 +10,13 @@ window.onload = function (){
 
     //configura a propriedade background do elemento
     p.style.backgroundColor = '#0000FF';
+
+    // captura o evento load da pagina
+    window.onload = function(){
+        //lcaliza elemento com id "id_p"
+        var links = p.gerElementsByTagName('a');
+
+        // alerta o atributo href do primeiro link
+        alert(links[0].href)
+    }
 }
